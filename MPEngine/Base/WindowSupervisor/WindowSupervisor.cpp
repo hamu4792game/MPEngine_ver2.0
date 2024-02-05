7a22745128cbb3a26ae3435284c86fe49100d705
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-02-05 15:55:47 +0900

    見つかった凡ミスの修正

--- MPEngine/Base/WindowSupervisor/WindowSupervisor.cpp
+++ MPEngine/Base/WindowSupervisor/WindowSupervisor.cpp
@@ -1,10 +1,10 @@
 #include "WindowSupervisor.h"
 //	imguiのinclude
-#include "externals/imgui/imgui.h"
-#include "externals/imgui/imgui_impl_dx12.h"
-#include "externals/imgui/imgui_impl_win32.h"
+#include "ImGuiManager/ImGuiManager.h"
+#ifdef _DEBUG
 //	関数の外部宣言
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+#endif // _DEBUG
 #include <string>
 
 #include "resource.h"
