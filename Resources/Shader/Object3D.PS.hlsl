632c68bd55ea42eac00f8e1cfcad489e9ae2bf53
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-02-05 12:07:07 +0900

    構成プロパティDemoの作成をしたが使えていないので失敗

--- Resources/Shader/Object3D.PS.hlsl
+++ Resources/Shader/Object3D.PS.hlsl
@@ -20,18 +20,19 @@ float4 main(VertexOutput input) : SV_TARGET {
         float cos = pow(NdotL * 0.5f + 0.5f,2.0f);
         diffuse = 
         textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        
+        float3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
+        //float3 halfVector = normalize(-gDirectionalLight.direction + toEye);
+        float3 reflectLight = reflect(gDirectionalLight.direction,normalize(input.normal));
+        //float NDotH = dot(normalize(input.normal),halfVector);
+        float RdotE = dot(reflectLight,toEye);
+        float specularPow = pow(saturate(RdotE),shininess); // 反射強度
+        float3 specular = 
+        gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float3(1.0f,1.0f,1.0f);
+
+        textureColor.rgb = diffuse + specular;
     }
     
-    float3 toEye = normalize(gCamera.worldPosition - input.worldPosition);
-    //float3 halfVector = normalize(-gDirectionalLight.direction + toEye);
-    float3 reflectLight = reflect(gDirectionalLight.direction,normalize(input.normal));
-    //float NDotH = dot(normalize(input.normal),halfVector);
-    float RdotE = dot(reflectLight,toEye);
-    float specularPow = pow(saturate(RdotE),shininess); // 反射強度
-    float3 specular = 
-    gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float3(1.0f,1.0f,1.0f);
-
-    textureColor.rgb = diffuse + specular;
     if (textureColor.a == 0.0) {
         discard;
     }
