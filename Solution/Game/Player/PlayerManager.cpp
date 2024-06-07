72c4d86984765696151ed3f7b667c4bc073dede7
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-06-07 17:07:25 +0900

    test

--- Solution/Game/Player/PlayerManager.cpp
+++ Solution/Game/Player/PlayerManager.cpp
@@ -16,7 +16,7 @@ void PlayerManager::Initialize() {
 	//global->LoadFile(itemName_);
 	
 	transform_.scale_ = Vector3::one;
-	//transform_.translation_ = Vector3(120.0f, 22.0f, -100.0f);
+	///transform_.translation_ = Vector3(120.0f, 22.0f, -100.0f);
 	transform_.translation_ = Vector3(0.0f, 22.0f, 0.0f);
 	transform_.UpdateMatrix();
 	animation_->Initialize();
