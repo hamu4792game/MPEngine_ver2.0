6f9a90ad1537f9279d5efc6f0ee62d01bcc5f696
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2024-06-19 16:42:46 +0900

    RadianBlurの実装

---
+++ Solution/Resources/Shader/RadialBlur.PS.hlsl
@@ -0,0 +1,34 @@
+#include "Fullscreen.hlsli"
+Texture2D<float4> gTexture : register(t0); // 通常テクスチャ
+SamplerState gSampler : register(s0); // サンプラー
+
+struct PixelShaderOutput {
+    float4 color : SV_TARGET0;
+};
+
+PixelShaderOutput main(VertexShaderOutput input) {
+    
+    const float32_t2 kCenter = float32_t2(0.5f,0.5f); // 中心点、ここを基準に放射状ブラーがかかる
+    const int32_t kNumSamples = 10; // サンプリング数。多いほど滑らかだが重い
+    const float32_t kBlurWidth = -0.01f; // ぼかしの幅。大きいほど大きい
+    // 中心から現在のUVに対しての方向を計算
+    // 普段方向といえば、単位ベクトルだが、ここではあえて正規化せず、遠いほどより遠くをサンプリングする
+    float32_t2 direction = input.texcoord - kCenter;
+    // 画面外に出ないようにするため、方向の制限
+    direction = clamp(direction, float32_t2(-0.5f, -0.5f), float32_t2(0.5f, 0.5f));
+    float32_t3 outputColor = float32_t3(0.0f,0.0f,0.0f);
+    for (int32_t sampleIndex = 0; sampleIndex < kNumSamples; ++sampleIndex) {
+        // 現在のUVからさきほど計算した方向にサンプリング点を進めながらサンプリングしていく
+        float32_t2 texcoord = input.texcoord + direction * kBlurWidth * float32_t(sampleIndex);
+        // texcoordの範囲内にクランプ、ラップを防ぐため
+        texcoord = clamp(texcoord, float32_t2(0.0f, 0.0f), float32_t2(1.0f, 1.0f));
+        outputColor.rgb += gTexture.Sample(gSampler,texcoord).rgb;
+    }
+    // 平均化する
+    outputColor.rgb *= rcp(kNumSamples);
+
+    PixelShaderOutput output;
+    output.color.rgb = outputColor;
+    output.color.a = 1.0f;
+    return output;
+}
