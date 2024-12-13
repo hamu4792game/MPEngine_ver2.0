8a102ea74812c5d9c9b8980ad552dbeba99660c9
hamuDesctop <shiki13275713@gmail.com>
2024-12-13 10:43:51 +0900

    animationCameraの追加前

---
+++ Solution/Game/Camera/AnimationCamera.h
@@ -0,0 +1,16 @@
+#pragma once
+//#include "MPEngine/Utils/Camera/Camera.h"
+#include "Utils/GlobalVariables/GlobalVariables.h"
+
+
+class AnimationCamera {
+	AnimationCamera() = default;
+	~AnimationCamera() = default;
+
+private:
+	struct KeyParam {
+		std::string key;
+	};
+
+
+};
