fddd5e7906077155b3ad338cf621e14ebc87f3c9
hamuDesctop <shiki13275713@gmail.com>
2025-01-22 03:34:30 +0900

    wireの描画修正。particleが想像以上にゴミなので要修正

--- Solution/Game/Wire/Wire.cpp
+++ Solution/Game/Wire/Wire.cpp
@@ -24,13 +24,23 @@ void Wire::Initialize(const Vector3& playerPos, const Vector3& anchor) {
 		SetWireTipModel();
 	}
 
+	// 色
+	for (uint32_t index = 0u; index < wireTipModels_->color_.size(); index++) {
+		if (index >= objectCount) {
+			wireTipModels_->color_.at(index) = Vector4(1.0f, 1.0f, 1.0f, 0.0f);
+		}
+		else {
+			wireTipModels_->color_.at(index) = Vector4(1.0f, 1.0f, 1.0f, 1.0f);
+		}
+	}
+
 	// オブジェクトに座標を割り当てる
 	Vector3 direction = FindVector(playerPos, anchor).Normalize();
 	uint32_t index = 0u;
 	Quaternion quat = Quaternion::MakeFromTwoVector(Vector3::front, direction);
 	for (auto& trans : wireTipModels_->transform_) {
 		// player座標から敷いていく
-		trans.translation_ = playerPos + (direction * (oneSize * index));
+		trans.translation_ = playerPos + (direction * (oneSize * index++));
 		trans.rotationQuat_ = quat;
 		trans.scale_ = Vector3::one;
 		trans.isQuaternion_ = true;
